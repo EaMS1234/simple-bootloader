93a96f0209b61b8e9582536ffce0ad86e0884da5
Erick Augusto <erickaugusto1234@gmail.com>
2025-01-06 16:41:18 -0300

    Kernel offset and Stack set up

--- src/boot.asm
+++ src/boot.asm
@@ -1,8 +1,25 @@
+; Real mode
 [bits 16]
+
+; Tells the assembler where to load the bootloader instructions in memory
+[org 0x7e00]
 
 mov ah, 0x0e
 mov al, '!'
 int 0x10
 
+; 4 kb kernel offset
+kernel equ 0x1000
+
+; Sets the stack up (64 kb offset)
+mov bp, 0x10000
+mov sp, bp
+
+; Infinite loop to keep the CPU running
+jmp $
+
+; Fills the rest of the sector with zeroes
 times 510 - ($-$$) db 0
+
+; Adds a word containing the BIOS' Magic Number in the end
 dw 0xaa55
