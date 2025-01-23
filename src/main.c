8bb782c84bb5e5dd69142ebc9ca60e60d764d905
Erick Augusto <erickaugusto1234@gmail.com>
2025-01-23 12:54:33 -0300

    Made a simple function for printing on screen

--- src/main.c
+++ src/main.c
@@ -1,4 +1,44 @@
+void print(char * string, int color);
+void next_char();
+void next_line();
+
+int term_x, term_y = 0;
+
 extern void main()
 {
-    *(char*)0xb8000 = '!';
+    print("Welcome to ", 7);
+    print("Simple-Bootloader", 3);
+    print("!\n", 7);
+    next_line();
+}
+
+void print(char * string, int color)
+{
+    char *video = (char*)0xb8000;
+
+    while (*string != 0)
+    {
+        if (*string == '\n')
+        {
+            next_line();
+            *string++;
+        }
+        else
+        {
+            *(video + (term_x * 2) + (term_y * 160)) = *string++;
+            *(video + (term_x * 2) + (term_y * 160) + 1) = color;
+            next_char();
+        }
+    }
+}
+
+void next_char()
+{
+    term_x += 1;
+}
+
+void next_line()
+{
+    term_x = 0;
+    term_y += 1;    
 }
