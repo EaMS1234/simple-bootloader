25e73889b839e81aa7950056e72e222445c70755
Erick Augusto <erickaugusto1234@gmail.com>
2025-01-24 12:09:11 -0300

    Update cursor position in text mode

--- src/lib/text.c
+++ src/lib/text.c
@@ -1,6 +1,9 @@
+#include "io.h"
+
 void print(char * string, int color);
 void next_char();
 void next_line();
+void update_cursor_pos();
 
 int term_x, term_y = 0;
 
@@ -22,6 +25,8 @@ void print(char * string, int color)
             next_char();
         }
     }
+
+    update_cursor_pos();
 }
 
 void next_char()
@@ -38,4 +43,25 @@ void next_line()
 {
     term_x = 0;
     term_y += 1;    
+
+    update_cursor_pos();
+}
+
+// Updates the VGA text mode cursor position by manipulating the values stored
+// on some of the internal registers
+void update_cursor_pos()
+{
+    int pos = term_x + (term_y * 80);
+
+    // Sends cursor location low register to the VGA control register
+    outb(0x3d4, 0x0f);
+
+    // Transforms pos into binary and uses the data register to change register 0x0f 
+	outb(0x3d5, (pos & 0xff));
+
+    // Sends cursor location high register to the VGA control register
+    outb(0x3d4, 0x0e);
+
+    // Transforms pos into binary and uses the data to change register 0x0e
+    outb(0x3d5, ((pos >> 8) & 0xff));
 }
