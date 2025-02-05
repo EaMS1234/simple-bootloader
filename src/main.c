73806e087cc24e360322da0df2cb8dd340987a73
Erick Augusto <erickaugusto1234@gmail.com>
2025-02-05 19:13:43 -0300

    Added code to get current keyboard scancode

--- src/main.c
+++ src/main.c
@@ -22,9 +22,21 @@ extern void main()
         // Responds with 0xfa + keyboard id
         inb(0x60);
         print_hex(inb(0x60), 7);
-        print(" (", 7);
+        print(" (ID ", 7);
         print_hex(inb(0x60), 7);
         print(") ", 7);
+
+        // Gets current scan code
+        outb(0x60, 0xf0);
+        outb(0x60, 0);
+        
+        // Displays current scancode if keyboard acknowledged the command
+        if (inb(0x60) != 0xfe)
+        {
+            print("(Scan code ", 7);
+            print_hex(inb(0x60), 7);
+            print(")", 7);
+        }
 
         print("\n", 7);
     }
